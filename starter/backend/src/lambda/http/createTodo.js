511e8509bf7bd24d07c3f3cb7bc6cb9a550161cb
Krarid <jmz.krarid@outlook.com>
2024-10-11 16:42:51 -0600

    feat: Create TODOs

--- starter/backend/src/lambda/http/createTodo.js
+++ starter/backend/src/lambda/http/createTodo.js
@@ -1,8 +1,40 @@
+import { DynamoDB } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
+import { v4 as uuidv4 } from 'uuid'
 
-export function handler(event) {
-  const newTodo = JSON.parse(event.body)
+const dynamoDbClient = DynamoDBDocument.from(new DynamoDB())
+const todosTable = process.env.TODOS_TABLE
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+export async function handler(event) {
+
+  console.log('Creating TODO: ', event)
+  const todoId = uuidv4()
+  // REFACTOR
+  const userId = uuidv4()
+
+  const parsedBody = JSON.parse(event.body)
+
+  const newTodo = {
+    todoId: todoId,
+    userId: userId,
+    ...parsedBody 
+  }
+
+  const createCommand = {
+    TableName: todosTable,
+    Item: newTodo
+  }
+
+  await dynamoDbClient.put(createCommand)
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      newTodo
+    })
+  }
 }
 
