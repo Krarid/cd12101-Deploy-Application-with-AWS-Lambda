f76381b64ce1e1fbf389ab21af6376dffd191c18
Krarid <jmz.krarid@outlook.com>
2024-10-12 18:54:07 -0600

    feat: Delete TODOs

--- starter/backend/src/lambda/http/deleteTodo.js
+++ starter/backend/src/lambda/http/deleteTodo.js
@@ -1,8 +1,32 @@
+import { DynamoDB } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
 
-export function handler(event) {
+const dynamoDbClient = DynamoDBDocument.from(new DynamoDB())
+const todosTable = process.env.TODOS_TABLE
+
+export async function handler(event) {
+
+  console.log('Deleting TODO: ', event)
+
   const todoId = event.pathParameters.todoId
+  const userId = '8e262baa-a694-4579-afa0-a596572aed5f'
 
-  // TODO: Remove a TODO item by id
-  return undefined
+  const deleteCommand = {
+    TableName: todosTable,
+    Key: { todoId, userId }
+  }
+
+  const result = await dynamoDbClient.delete(deleteCommand)
+  const items = result.Items
+
+  return {
+    statusCode: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items
+    })
+  }
 }
 
